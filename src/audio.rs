0bfbf80008c590e5002e92ad0ec9470b2de9f03e
Eduardo Pinho <enet4mikeenet@gmail.com>
2024-07-26 08:33:27 +0100

    Refactor audio
    - move audio stuff to module

---
+++ src/audio.rs
@@ -0,0 +1,132 @@
+use dos_x::djgpp::{dos::delay, pc::inportb};
+
+static mut NO_SOUND: bool = false;
+
+// Hz
+const PIT_FREQUENCY: u32 = 0x1234DD;
+
+// helper macro for defining countdowns of notes as constants
+macro_rules! const_note {
+    ($name: ident, $freq: literal) => {
+        const $name: u16 = (PIT_FREQUENCY / $freq) as u16;
+    };
+}
+
+/// disable sound
+pub fn sound_off() {
+    unsafe {
+        NO_SOUND = true;
+    }
+}
+
+pub fn is_sound_on() -> bool {
+    unsafe {
+        !NO_SOUND
+    }
+}
+
+/// Play a tune using PC speaker
+pub fn play_tune() {
+    use dos_x::djgpp::pc::outportb;
+
+    if unsafe { NO_SOUND } {
+        return;
+    }
+
+    // setting up a few constants for note countdowns
+    const_note!(NOTE_A5, 880);
+    const_note!(NOTE_CS6, 1109);
+    const_note!(NOTE_D6, 1175);
+    const_note!(NOTE_E6, 1328);
+    const_note!(NOTE_GS6, 1661);
+    const_note!(NOTE_A6, 1760);
+    const_note!(NOTE_B6, 1975);
+
+    const NOTE_VOID: u16 = 3;
+
+    #[inline]
+    unsafe fn play_note(countdown: u16) {
+        outportb(0x42, (countdown & 0xff) as u8);
+        outportb(0x42, (countdown >> 8) as u8);
+    }
+
+    // use PC speaker
+    unsafe {
+        let inb = inportb(0x61);
+        outportb(0x61, inb | 3); // enable speaker
+        outportb(0x43, 0xb6); // set PIT
+
+        // String Quintet in E Major, Op. 11, No. 5, G. 275: III. Minuetto by Boccherini
+        play_note(NOTE_A6);
+        delay(75);
+        play_note(NOTE_B6);
+        delay(75);
+        play_note(NOTE_A6);
+        delay(140);
+        play_note(NOTE_GS6);
+        delay(140);
+        play_note(NOTE_A6);
+        delay(140);
+        play_note(NOTE_B6);
+        delay(140);
+        play_note(NOTE_A6);
+        delay(140);
+
+        play_note(NOTE_VOID);
+        delay(140);
+
+        play_note(NOTE_A5);
+        delay(280);
+
+        play_note(NOTE_VOID);
+        delay(280);
+
+        play_note(NOTE_CS6);
+        delay(280);
+
+        play_note(NOTE_VOID);
+        delay(280);
+
+        play_note(NOTE_E6);
+        delay(140);
+        play_note(NOTE_VOID);
+        delay(140);
+        play_note(NOTE_E6);
+        delay(280);
+        play_note(NOTE_D6);
+        delay(140);
+        play_note(NOTE_VOID);
+        delay(140);
+        play_note(NOTE_D6);
+        delay(280);
+
+        // turn off
+        outportb(0x61, 0);
+        delay(280);
+    }
+}
+
+/// Play a click sound
+pub fn play_click() {
+    use dos_x::djgpp::pc::outportb;
+
+    if unsafe { NO_SOUND } {
+        return;
+    }
+
+    // use PC speaker
+    unsafe {
+        let inb = inportb(0x61);
+        outportb(0x61, inb | 3); // enable speaker
+        outportb(0x43, 0xb6); // set PIT
+
+        // a note played fast enough will sound like a click
+        let countdown = 1500;
+        outportb(0x42, (countdown & 0xff) as u8);
+        outportb(0x42, (countdown >> 8) as u8);
+        delay(4);
+
+        // turn off
+        outportb(0x61, 0);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,11 @@
 #![no_main]
 extern crate alloc;
 
+mod audio;
 mod tiles;
 
+use audio::{is_sound_on, play_click, play_tune, sound_off};
 use dos_x::djgpp::dos::delay;
-use dos_x::djgpp::pc::inportb;
 use dos_x::key;
 use dos_x::vga::Palette;
 use tiles::{Move, Tiles};
@@ -21,8 +22,6 @@ static IMAGE_1_DATA: &[u8] = include_bytes!("../resources/1.png");
 static IMAGE_2_DATA: &[u8] = include_bytes!("../resources/2.png");
 static IMAGE_3_DATA: &[u8] = include_bytes!("../resources/3.png");
 
-static mut NO_SOUND: bool = false;
-
 #[no_mangle]
 fn dos_main() {
     // process inputs
@@ -32,7 +31,7 @@ fn dos_main() {
         unsafe {
             let arg = core::ffi::CStr::from_ptr(*arg);
             if arg.to_bytes() == b"nosound" {
-                NO_SOUND = true;
+                sound_off();
             } else if arg.to_bytes() == b"iknowwhatimdoing" {
                 starting_level = 2;
             } else {
@@ -139,10 +138,10 @@ fn run(mut rng: impl RandRange<u16>, starting_level: u8) {
                     dos_x::vga::draw_buffer(&image.pixels());
                 }
                 unsafe {
-                    if NO_SOUND {
-                        delay(1_500);
-                    } else {
+                    if is_sound_on() {
                         play_tune();
+                    } else {
+                        delay(1_500);
                     }
                 }
 
@@ -409,121 +408,5 @@ fn handle_panic(_info: &PanicInfo) -> ! {
         // exit using libc
         exit(-1);
         core::hint::unreachable_unchecked()
-    }
-}
-
-// Hz
-const PIT_FREQUENCY: u32 = 0x1234DD;
-
-// helper macro for defining countdowns of notes as constants
-macro_rules! const_note {
-    ($name: ident, $freq: literal) => {
-        const $name: u16 = (PIT_FREQUENCY / $freq) as u16;
-    };
-}
-
-/// Play a tune using PC speaker
-fn play_tune() {
-    use dos_x::djgpp::pc::outportb;
-
-    if unsafe { NO_SOUND } {
-        return;
-    }
-
-    // setting up a few constants for note countdowns
-    const_note!(NOTE_A5, 880);
-    const_note!(NOTE_CS6, 1109);
-    const_note!(NOTE_D6, 1175);
-    const_note!(NOTE_E6, 1328);
-    const_note!(NOTE_GS6, 1661);
-    const_note!(NOTE_A6, 1760);
-    const_note!(NOTE_B6, 1975);
-
-    const NOTE_VOID: u16 = 3;
-
-    #[inline]
-    unsafe fn play_note(countdown: u16) {
-        outportb(0x42, (countdown & 0xff) as u8);
-        outportb(0x42, (countdown >> 8) as u8);
-    }
-
-    // use PC speaker
-    unsafe {
-        let inb = inportb(0x61);
-        outportb(0x61, inb | 3); // enable speaker
-        outportb(0x43, 0xb6); // set PIT
-
-        // String Quintet in E Major, Op. 11, No. 5, G. 275: III. Minuetto by Boccherini
-        play_note(NOTE_A6);
-        delay(75);
-        play_note(NOTE_B6);
-        delay(75);
-        play_note(NOTE_A6);
-        delay(140);
-        play_note(NOTE_GS6);
-        delay(140);
-        play_note(NOTE_A6);
-        delay(140);
-        play_note(NOTE_B6);
-        delay(140);
-        play_note(NOTE_A6);
-        delay(140);
-
-        play_note(NOTE_VOID);
-        delay(140);
-
-        play_note(NOTE_A5);
-        delay(280);
-
-        play_note(NOTE_VOID);
-        delay(280);
-
-        play_note(NOTE_CS6);
-        delay(280);
-
-        play_note(NOTE_VOID);
-        delay(280);
-
-        play_note(NOTE_E6);
-        delay(140);
-        play_note(NOTE_VOID);
-        delay(140);
-        play_note(NOTE_E6);
-        delay(280);
-        play_note(NOTE_D6);
-        delay(140);
-        play_note(NOTE_VOID);
-        delay(140);
-        play_note(NOTE_D6);
-        delay(280);
-
-        // turn off
-        outportb(0x61, 0);
-        delay(280);
-    }
-}
-
-/// Play a click sound
-fn play_click() {
-    use dos_x::djgpp::pc::outportb;
-
-    if unsafe { NO_SOUND } {
-        return;
-    }
-
-    // use PC speaker
-    unsafe {
-        let inb = inportb(0x61);
-        outportb(0x61, inb | 3); // enable speaker
-        outportb(0x43, 0xb6); // set PIT
-
-        // a note played fast enough will sound like a click
-        let countdown = 1500;
-        outportb(0x42, (countdown & 0xff) as u8);
-        outportb(0x42, (countdown >> 8) as u8);
-        delay(4);
-
-        // turn off
-        outportb(0x61, 0);
     }
 }
